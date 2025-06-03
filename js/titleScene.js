bdf17d0451cd9f84937bfb42dd2a4a220642c8eb
Catherine Vu <catherine.vu@stu.ocsb.ca>
2025-06-03 02:58:57 +0000

    edit title scene js

--- js/titleScene.js
+++ js/titleScene.js
@@ -16,6 +16,12 @@ class TitleScene extends Phaser.Scene {
   constructor() {
     super({ key: "titleScene" })
     this.titleSceneBackgroundImage = null
+    this.titleSceneText = null
+    this.titleSceneTextStyle = { 
+      font: "200px Times", 
+      fill: "#fde4b9", 
+      align: "center"
+    }
   }
 
   /**
@@ -45,6 +51,13 @@ class TitleScene extends Phaser.Scene {
 
   creat(data) {
     //pass
+    this.titleSceneBackgroundImage = this.add
+    .sprite(0, 0, "titleSceneBackground")
+    .setScale(2.75)
+    this.titleSceneBackgroundImage.x = 1920 / 2
+    this.titleSceneBackgroundImage.y = 1920 / 2
+
+    this.titleSceneText = this.add.text(1920 / 2, (1080 / 2) + 350, "Space Aliens").setOrigin(0.5)
   }
 
   /**Should be overridden by your Own Scenes. 
